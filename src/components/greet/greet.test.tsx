ae68279df5b95bd79e8dcf54ab312742de73590f
Priya <absapriya@gmail.com>
2023-07-14 15:41:45 +0530

    initial commit

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,13 @@
+import { Greet } from "./greet"
+import { render,screen } from "@testing-library/react"
+test('Greet renders correctly',()=>{
+    render(<Greet/>)
+   const textElement= screen.getByText('Hello')
+   expect(textElement).toBeInTheDocument()
+})
+test('Greet renders with a name',()=>{
+    render(<Greet name="Priya"/>)
+    const textElement=screen.getByText('Hello Priya')
+    expect(textElement).toBeInTheDocument();
+
+})
