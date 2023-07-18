cf2eb684df15debb911d912cfd7710b5a3e56cf7
Priya <absapriya@gmail.com>
2023-07-18 11:41:50 +0530

    getbyquries commit

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -12,19 +12,38 @@ describe('Application',()=>{
             level:2
         })
         expect(sectionHeading).toBeInTheDocument();
+        const paragraphelement=screen.getByText('All Fielads are mandatory');
+        expect(paragraphelement).toBeInTheDocument();
        const nameElement= screen.getByRole('textbox',{
         name:'Name'
        })
+       const closeElement=screen.getByTitle('close')
+       expect(closeElement).toBeInTheDocument();
+       const imageElement=screen.getByAltText('a person with a laptop');
+       expect(imageElement).toBeInTheDocument();
+       const customElemnt=screen.getByTestId('custom-element')
+       expect(customElemnt).toBeInTheDocument();
        expect(nameElement).toBeInTheDocument();
+       const nameElement2=screen.getByLabelText('Name',{
+        selector:'input'
+       });
+       expect(nameElement2).toBeInTheDocument();
+       const nameElement3=screen.getByPlaceholderText('FullName');
+       expect(nameElement3).toBeInTheDocument();
+       const nameElement4=screen.getByDisplayValue('priya');
+       expect(nameElement4).toBeInTheDocument();
        const bioElement=screen.getByRole('textbox',{
         name:"Bio"
        })
+
        expect(bioElement).toBeInTheDocument();
        expect(nameElement).toBeInTheDocument()
        const jobLocationElement=screen.getByRole('combobox')
        expect(jobLocationElement).toBeInTheDocument()
        const termselement=screen.getByRole('checkbox')
        expect(termselement).toBeInTheDocument()
+       const termsElement2=screen.getByLabelText('I agree to the terms and conditions')
+       expect(termsElement2).toBeInTheDocument();
        const submitButtonElement=screen.getByRole('button')
        expect(submitButtonElement).toBeInTheDocument()
     })
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -5,11 +5,16 @@ export const Application = () => {
     <>
     <h1>Job Application Form</h1>
     <h2>Section 1</h2>
-    
+    <p>All Fielads are mandatory</p>
+    <span title='close'>X</span>
+    <img src="https://via.placeholder.com/150" alt="a person with a laptop" />
+    <div data-testid='custom-element'>
+        Custom HTML Elemnt
+    </div>
     <form>
         <div>
             <label htmlFor='name'>Name</label>
-            <input type='text' id='name'/>
+            <input type='text' id='name' value='priya' onChange={()=>{}} placeholder='FullName'/>
             
         </div>
         <div>
@@ -18,7 +23,7 @@ export const Application = () => {
             
         </div>
         <div>
-        <label htmlFor='job-location'>JobLocation</label>
+        <label htmlFor='job-location'>Name</label>
         <select id='job-location'>
             <option value="">Select One</option>
             <option value="IND">India</option>
